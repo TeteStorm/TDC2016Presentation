9c90c5f91f66cfa0372e1ae2ed3ffe6339a49a55
Tan Storm <tanstormandrade@gmail.com>
2016-10-07 13:16:55 -0300

    add

--- PatternMaching/Program.cs
+++ PatternMaching/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Domain;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,312 +9,102 @@ namespace PatternMaching
 {
     class Program
     {
-        public static void ExibirInformacoesCotacao(Cotacao cotacao)
-        {
-            //double valorCotacao = cotacao match
-            //              (
-            //                  case CotacaoDolar dolar: dolar.ValorComercial
-            //                  case CotacaoEuro euro: euro.ValorCotacao
-            //                  case * : 0
-            //              );
-
-
-            double valorCotacao = 0;
-            if (cotacao is CotacaoDolar dolar)
-                valorCotacao = dolar.ValorComercial;
-            else if (cotacao is CotacaoEuro euro)
-                valorCotacao = euro.ValorCotacao;
-
-            Console.WriteLine(new String('-', 40));
-            Console.WriteLine($"Data: {cotacao.DataCotacao:dd/MM/yyyy}");
-            Console.WriteLine($"Sigla: {cotacao.SiglaMoeda}");
-            Console.WriteLine($"Moeda: {cotacao.NomeMoeda}");
-            Console.WriteLine($"Valor: {valorCotacao:0.0000}");
-        }
 
         static void Main(string[] args)
         {
-            CotacaoDolar dolar = new CotacaoDolar();
-            dolar.DataCotacao = new DateTime(2016, 4, 29);
-            dolar.ValorComercial = 3.440;
-            dolar.ValorTurismo = 3.580;
-            ExibirInformacoesCotacao(dolar);
-
-            CotacaoEuro euro = new CotacaoEuro();
-            euro.DataCotacao = new DateTime(2016, 4, 29);
-            euro.ValorCotacao = 3.938;
-            ExibirInformacoesCotacao(euro);
+           
 
             Console.ReadKey();
         }
 
-        public static void CalculateBodyFatCotacao(BodyFatSkinfoldProtocol bodyFatProtocol)
+        public void PrintProtocolName(object o)
         {
-            //double valorCotacao = cotacao match
-            //              (
-            //                  case CotacaoDolar dolar: dolar.ValorComercial
-            //                  case CotacaoEuro euro: euro.ValorCotacao
-            //                  case * : 0
-            //              );
+            if (o is null) return;     // constant pattern "null"
+            if (!(o is int i))
+            { Console.WriteLine(new string('*', i)); }
+            return; // type pattern "int i"
+           
+        }
 
 
+        public static void CalculateBodyFat(BodyFatSkinfoldProtocol bodyFatProtocol)
+        {
             decimal bodyFat = 0;
+            var bodyFatPercent = 0D;
+
+            //Utilização do "is"
+
+            // Constant pattern
+            // Antes podíamos validar se o valor era nulo ou se o valor era de um determinado tipo
+            // if (bodyFatProtocol == null) { ... }
+            // if (bodyFatProtocol is BodyFatSkinfoldProtocol) { ... }
+            // Agora podemos validar diretamente em constantes (ou números)
+            if (bodyFatProtocol is null)
+                throw new ArgumentNullException("Protocol must not be null");
+
+            // Var pattern
+            // Aqui sempre vai dar match, a comparação simplemente coloca o valor obtido 
+            // numa nova variável que será do tipo do input, vale lembrar que ela funciona somente dentro do scopo do if
+            if (bodyFatPercent is var bf)
+            {
+                Console.WriteLine(bf);
+            }
+
+            // Type pattern
+            // Aqui além de testar o tipo extraímos o valor em uma nova variável do tipo proposto
             if (bodyFatProtocol is Guedes protocol)
             {
+                //{ ... }
+                bodyFat = protocol.Height;
+            }   
+            else if (bodyFatProtocol is Faulkner protocol)
+            {
+                //{ ... }
                 bodyFat = protocol.Height;
             }
-                
-            else if (bodyFatProtocol is Faulkner protocol)
-                bodyFat = protocol.Height;
             else if (bodyFatProtocol is PollockFive protocol)
+            {
+                //{ ... }
                 bodyFat = protocol.Height;
+            }
             else if (bodyFatProtocol is PollockSeven protocol)
+            {
+                //{ ... }
                 bodyFat = protocol.Height;
+            }
             else if (bodyFatProtocol is Yuhasz protocol)
+            {
+                //{ ... }
                 bodyFat = protocol.Height;
+            }
             else if (bodyFatProtocol is Lohman protocol)
+            {
+                //{ ... }
                 bodyFat = protocol.Height;
+            }
+
 
             Console.WriteLine($"Used Protocol : {bodyFatProtocol.ProtocolName}");
             Console.WriteLine($"Valor: {bodyFat:0.0000}");
         }
 
+        public static void PrintRoules(BodyFatSkinfoldProtocol bodyFatProtocol)
+        {
+            //Utilização do swith
+            switch (bodyFatProtocol)
+            {
+                case Faulkner faulkner when faulkner.Age > 18:
+                    Console.WriteLine($"Used Protocol Formule : {bodyFatProtocol.FormuleDescription}");
+                    break;
+                case Guedes guedes:
+                    Console.WriteLine(guedes.FormuleDescription);
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
     }
 
-    public enum Genre
-    {
-        Female = 0,
-        Male = 1
-    }
-
-
-    public abstract class BodyFatSkinfoldProtocol
-    {
-        public abstract string ProtocolName { get; }
-
-        public abstract int SkinfoldCount { get; }
-
-        public abstract string FormuleDescription { get; }
-
-        public Genre Sex { get; set; } 
-
-
-        public decimal Height { get; set; }
-
-
-        public decimal Weight { get; set; }
-
-        public decimal Tricep { get; set; }
-    }
-
-    public class Guedes : BodyFatSkinfoldProtocol
-    {
-        public int Age { get; set; }
-        public override string FormuleDescription
-        {
-            get
-            {
-                return "";
-            }
-        }
-
-        public override string ProtocolName
-        {
-            get
-            {
-                return "Guedes";
-            }
-        }
-
-        public override int SkinfoldCount
-        {
-            get
-            {
-                return 2;
-            }
-        }
-
-        public decimal Subscapular { get; set; }
-    }
-
-    public class Faulkner : Guedes
-    {
-        public override string FormuleDescription
-        {
-            get
-            {
-                return "";
-            }
-        }
-
-        public override string ProtocolName
-        {
-            get
-            {
-                return "Faulkner";
-            }
-        }
-
-        public override int SkinfoldCount
-        {
-            get
-            {
-                return 4;
-            }
-        }
-        public decimal Abdominal { get; set; }
-        public decimal Suprailiac { get; set; }
-    }
-
-    public class PollockFive : BodyFatSkinfoldProtocol
-    {
-        public override string FormuleDescription
-        {
-            get
-            {
-                return "5 Dobras cutâneas (DC): Tríceps; coxa; supra-ilíaca; abdome e peitoral (X1=somatória de peitoral, abdome e coxa; X2=somatória de tríceps, supra-ilíaca e coxa, X3= idade em anos)";
-            }
-        }
-
-        public override string ProtocolName
-        {
-            get
-            {
-                return "Pollock Five";
-            }
-        }
-
-        public override int SkinfoldCount
-        {
-            get
-            {
-                return 5;
-            }
-        }
-        public int Age { get; set; }
-        public decimal Abdominal { get; set; }
-        public decimal Suprailiac { get; set; }
-        public decimal Chest { get; set; }
-
-        public decimal Thigh { get; set; }
-    }
-
-    public class PollockSeven : PollockFive
-    {
-        public override string FormuleDescription
-        {
-            get
-            {
-                return "Dobras cutâneas (DC): Subescapular, axilar média, tríceps; coxa; supra-ilíaca; abdome e peitoral (ST= soma de todas)";
-            }
-        }
-
-        public override string ProtocolName
-        {
-            get
-            {
-                return "Pollock Seven";
-            }
-        }
-
-        public override int SkinfoldCount
-        {
-            get
-            {
-                return 7;
-            }
-        }
-        public decimal Midaxilary { get; set; }
-
-        public decimal Subscapular { get; set; }
-    }
-
-    public class Yuhasz : Faulkner
-    {
-        public override string FormuleDescription
-        {
-            get
-            {
-                return " 6 Dobras cutâneas (DC): Subescapular, tríceps; coxa; supra-ilíaca; abdome e peitoral (S6=somatória de todas)";
-            }
-        }
-
-        public override string ProtocolName
-        {
-            get
-            {
-                return "Yuhasz";
-            }
-        }
-
-        public override int SkinfoldCount
-        {
-            get
-            {
-                return 6;
-            }
-        }
-        public decimal Chest { get; set; }
-    }
-
-    public class Lohman : BodyFatSkinfoldProtocol
-    {
-        public override string FormuleDescription
-        {
-            get
-            {
-                return " 2 Dobras cutâneas (DC): Tríceps e Perna";
-            }
-        }
-
-        public override string ProtocolName
-        {
-            get
-            {
-                return "Lohman";
-            }
-        }
-
-        public override int SkinfoldCount
-        {
-            get
-            {
-                return 2;
-            }
-        }
-        public int Age { get; set; }
-
-
-        public decimal Thigh { get; set; }
-    }
-
-    public abstract class Cotacao
-    {
-        public DateTime DataCotacao { get; set; }
-        public abstract string SiglaMoeda { get; }
-        public abstract string NomeMoeda { get; }
-    }
-
-    public class CotacaoDolar : Cotacao
-    {
-        public override string SiglaMoeda
-        { get { return "USD"; } }
-
-        public override string NomeMoeda
-        { get { return "Dólar norte-americano"; } }
-
-        public double ValorComercial { get; set; }
-        public double ValorTurismo { get; set; }
-    }
-
-    public class CotacaoEuro : Cotacao
-    {
-        public override string SiglaMoeda
-        { get { return "EUR"; } }
-
-        public override string NomeMoeda
-        { get { return "Euro"; } }
-
-        public double ValorCotacao { get; set; }
-    }
 }
--- Tuples/Program.cs
+++ Tuples/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Domain;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,13 +12,76 @@ namespace Tuples
     {
         static void Main(string[] args)
         {
+            PrintDesconstruction();
+           // PrintProtocolDetais();
+           // PrintNamedProtocolDetais();
         }
 
-        static (int count, string value) Foo()
+        // Tuples and Desconstruction
+
+        // Retorna 3 valores dentro de um elemento tupla que podem ser acessados atráves das propriedades Item1, Item2, Item3
+        static (decimal, decimal, string) GetProtocolValues(BodyFatSkinfoldProtocol bodyFatProtocol)
         {
-            var retVal = (c: 1, v: "Foo");
-            return retVal;
+            return (bodyFatProtocol.Weight, bodyFatProtocol.BodyFatPercent, bodyFatProtocol.ProtocolName);
         }
 
+        static void PrintProtocolDetais()
+        {
+            var protocolDetails = GetProtocolValues(new Guedes() { Height = 167, Weight = 58 });
+            Console.WriteLine($"Weight:{protocolDetails.Item1} Total Body Fat:{protocolDetails.Item2} Protocol:{protocolDetails.Item3}");
+        }
+
+
+        // Retorna 3 valores dentro de um elemento tupla mas de maneira nomeada com variáveis explícitas
+        // que poderão ser acessadas atráves dos nomes das propriedades
+        static (decimal Weight, decimal TotalBodyFat, string Protocol) GetNamedProtocolValues(BodyFatSkinfoldProtocol bodyFatProtocol)
+        {
+            var returnValues = (bodyFatProtocol.Weight, bodyFatProtocol.BodyFatPercent, bodyFatProtocol.ProtocolName);
+            return returnValues;
+        }
+
+
+        //Desconstruction
+        static void PrintNamedProtocolDetais()
+        {
+            //Em variáveis pré declaradas
+            decimal weight, totalFat;
+            string protocol;
+
+            (weight, totalFat, protocol) = GetNamedProtocolValues(new Guedes() { Height = 167, Weight = 58 });
+            
+            // Em uma variável anônima
+            var protocolDetails = GetNamedProtocolValues(new Guedes() { Height = 167, Weight = 58 });        
+            Console.WriteLine($"Weight:{protocolDetails.Weight} Total Body Fat:{protocolDetails.TotalBodyFat} Protocol:{protocolDetails.Protocol}");
+
+            // Em variáveis explícitas 
+            (var we, var bft, var pn) = GetNamedProtocolValues(new Guedes() { Height = 167, Weight = 58 });
+            Console.WriteLine($"Weight:{we} Total Body Fat:{bft} Protocol:{pn}");
+
+            // Em variáveis explícitas tipadas
+            (decimal w, decimal bf, string p) = GetNamedProtocolValues(new Guedes() { Height = 167, Weight = 58 });
+            Console.WriteLine($"Weight:{w} Total Body Fat:{bf} Protocol:{p}");
+
+        }
+
+        // Retorna 3 valores dentro de um elemento literal de tupla de maneira nomeada que podem ser acessados atráves dos nomes das propriedades 
+        // especificados mas somente dentro do scopo onde está sendo criado, externamente só acessaríamos novamente por Item1, Item2, Item3
+        static (decimal, decimal , string ) GetLiteralProtocolValues(BodyFatSkinfoldProtocol bodyFatProtocol)
+        {
+            var returnValues = (Weight: bodyFatProtocol.Weight, TotalBodyFat: bodyFatProtocol.BodyFatPercent, Protocol: bodyFatProtocol.ProtocolName);
+            Console.WriteLine($"Weight:{returnValues.Weight} Total Body Fat:{returnValues.TotalBodyFat} Protocol:{returnValues.Protocol}");
+            return returnValues;
+        }
+
+
+        // Deconstruct
+        // Automaticamente chama o método ou extension method Desconstruct implementado no objeto
+        public static void PrintDesconstruction()
+        {
+            var guedes = new Guedes() { Height = 167, Weight = 58 };
+            (var name, var description) = guedes;
+            Console.WriteLine($"Protocol name: {name}");
+            Console.WriteLine($"Protocol description: {description}");
+        }
     }
 }
